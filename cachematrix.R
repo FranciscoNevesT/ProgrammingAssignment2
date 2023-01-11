46f267166a9707809707948a6cb1198e3bcde9ba
francisco neves <francisconevest@outlook.com>
2023-01-11 15:00:09 -0300

    Implementing the cache matrix

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,43 @@
 
 ## Write a short comment describing this function
 
+
+##Create a list represented a cache matrix
 makeCacheMatrix <- function(x = matrix()) {
+  s <- NULL
+  
+  set <- function(y){
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+  
 
 }
 
 
 ## Write a short comment describing this function
 
+##Aplly solve in the list (cache matrix)
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  s <- x$getsolve()
+  
+  if(!is.null(s)){
+    message("getting chached data")
+    return(s)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  
+  x$setsolve(m)
+  m
+  
 }
